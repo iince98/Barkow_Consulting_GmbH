345c3e9d0fc17b4b9de8d20bdad799443f1695f0
Ibrahim Ince <iince98@gmail.com>
2023-06-21 20:58:39 +0200

    deneme

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 a=[1,2,3,4,5]
 b=[12,16,3,15,4]
 st.header ("Section Start")
-st.text ("Bugün bayram")
+st.text ("Bugün bayramdir")
 fig, ax = plt.subplots(1,1)
 ax.scatter (x=a, y=b)
 st.pyplot(fig)
