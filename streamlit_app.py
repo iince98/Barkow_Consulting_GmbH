71d020e08f0e5b0e292798fd82a7964db4c77d8b
Ibrahim Ince <iince98@gmail.com>
2023-06-21 21:02:37 +0200

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 a=[1,2,3,4,5]
 b=[12,16,3,15,4]
 st.header ("Section Start")
-st.text ("Bugün bayramdir")
+st.text ("Bugün bayramdir1")
 fig, ax = plt.subplots(1,1)
 ax.scatter (x=a, y=b)
 ax.set_xlabel("x ekseni")
