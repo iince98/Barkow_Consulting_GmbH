093a9a215e6a26bd890b2a1ad772801ccf7ea3ec
I INCE <iince98@gmail.com>
2023-06-21 20:51:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 a=[1,2,3,4,5]
 b=[12,16,3,15,4]
 st.header ("Section Start")
+st.text ("Bugün bayram")
 fig, ax = plt.subplots(1,1)
 ax.scatter (x=a, y=b)
 st.pyplot(fig)
