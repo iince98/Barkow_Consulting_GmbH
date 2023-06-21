da10902e2fd55fcceee9ab9ef69a94f377c83306
Ibrahim Ince <iince98@gmail.com>
2023-06-21 22:30:02 +0200

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,9 @@ for pfm in possible_platforms:
 st.markdown("Report Category Specific Parameters")
 
 st.multiselect("Platform", possible_platforms)
-
-st.markdown(f'<style>div.css-1wivap2.e1vioofd3 {background-color:"red";}</style>',unsafe_allow_html=True)
+with open ('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+    
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
