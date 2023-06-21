cee7d6d08ed64892ec22342381e88c68b46ee390
Ibrahim Ince <iince98@gmail.com>
2023-06-21 21:35:11 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ st.markdown("Report Category Specific Parameters")
 st.multiselect("Platform", possible_platforms)
 
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(4)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
