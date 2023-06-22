7b500fe450f160c1af401552a26db2a695ea7eb8
Ibrahim Ince <iince98@gmail.com>
2023-06-22 22:47:00 +0200

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ with open ('style.css') as f:
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col3.metric("Humidity", "86%", "4%","2")
