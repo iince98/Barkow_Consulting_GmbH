59f7a071b197a7b6fad8f52671e8b06524ac94c7
Ibrahim Ince <iince98@gmail.com>
2023-06-24 19:39:18 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,22 +61,11 @@ plt.legend(loc="upper left")
 
 
 
-
-
-
-#st.pyplot(fig)
 fig_html = mpld3.fig_to_html(fig)
 components.html(fig_html, height=600)
-
-possible_platforms = ["Carousell", "Foo", "Bar1"]
 
 
 with open ('style.css') as file1:
     st.markdown(f'<style>{file1.read()}</style>',unsafe_allow_html=True)
     st.text (file1.read())
 
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
