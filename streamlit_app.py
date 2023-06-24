faaa6912130a7747c1faae7e33713a16cbbdf1b9
Ibrahim Ince <iince98@gmail.com>
2023-06-24 17:22:44 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ df = pd.read_csv('son.csv', delimiter=';')
 #df['birlesik']=df['Year']+df['Quarter']
 
 # = df['Year'].astype(str) + "_" + df['Quarter']
-df['Quarter'] = df['Quarter'].apply(lambda x: int(str(x)[:-1])*3)
+df['Quarter'] = df['Quarter'].apply(lambda x: str(x)[:-1])
 
 df['Year'] = df['Year'].apply(lambda x: pd.to_datetime(str(x), format='%Y'))
 df['quar'] = pd.PeriodIndex(df.Year, freq='Q')
@@ -43,7 +43,7 @@ st.text (df.describe())
 
 st.header ("Section Start")
 
-st.text(df['Quarter'][1])
+st.text(df['Quarter'][0])
 
 
 fig, ax = plt.subplots(1,1)
