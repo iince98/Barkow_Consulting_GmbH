c6e976a6cbb88ab916d2014c713fd71b476c36bc
Ibrahim Ince <iince98@gmail.com>
2023-06-25 00:27:29 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,7 @@ mortgage = df['mortgage_rates']
 
 
 st.markdown(
-   f'''
-   <style>
-   p {
-   background-image: url(imza_copy.png’);
-   }
-   </style>
-   ''',
-   unsafe_allow_html=True)
+   f'<style>p {background-image: url("imza_copy.png");}</style>',unsafe_allow_html=True)
 #image = Image.open('imza_copy.png')
 #st.image(image, caption='Barkow Consulting GmbH')
 
