3b4ec3b51838dd08320a884b996a9bc89660e33d
Ibrahim Ince <iince98@gmail.com>
2023-06-27 00:19:07 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,10 @@ hist_data = [x1, x2, x3]
 group_labels = ['Group 1', 'Group 2', 'Group 3']
 
 # Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+fig2 = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
 
 # Plot!
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig2, use_container_width=True)
 
 
 
