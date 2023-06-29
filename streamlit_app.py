abcd69a733ac48bbe5d3e25d5f2e372919c447d9
Ibrahim Ince <iince98@gmail.com>
2023-06-29 05:52:58 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,22 @@ from PIL import Image
 import numpy as np
 import plotly.figure_factory as ff
 
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('son.csv', delimiter=';')
+#df['birlesik']=df['Year']+df['Quarter']
+df['Quarter'] = df['Quarter'].apply(lambda x: str(int(x.split()[1])*3)) 
+for i in range (len(df['Year'])):
+    df['Year'][i] = str(df['Year'][i]) + df['Quarter'][i]
+df['Year'] = df['Year'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m'))
+year = df['Year']
+const = df['Construction_costs']
+material = df['Material_costs']
+labour = df['Labour_costs']
+house_price = df['House_price_index']
+new_residential = df['New_residential_property']
+existing_residential = df['Existing_residential_property']
+mortgage = df['mortgage_rates']
+
 def introduction():
 
     st.header ("Task")
@@ -37,35 +53,18 @@ def introduction():
     # Plot!
     st.plotly_chart(fig, use_container_width=True)    
 
-def b():    
-    pd.set_option('display.max_columns', None)
-    df = pd.read_csv('son.csv', delimiter=';')
-    #df['birlesik']=df['Year']+df['Quarter']
+def Dataset(): 
+    st.header("Data set")
+    st.dataframe(df)
+    st.markdown ("Source: https://www.destatis.de/EN/Themes/Economy/Prices/Construction-Prices-And-Real-Property-Prices/_node.html#267504")
+    st.markdown ("https://www.destatis.de/en/Themes/Economy/Prices/Construction-Prices-And-Real-Property-Prices/_node.html")
+    st.markdown ("https://www.bundesbank.de/dynamic/action/en/statistics/time-series-databases/time-series-databases/745582/745582?tsId=BBK01.SUD131&dateSelect=2023")
 
-
-    df['Quarter'] = df['Quarter'].apply(lambda x: str(int(x.split()[1])*3)) 
-
-    for i in range (len(df['Year'])):
-        df['Year'][i] = str(df['Year'][i]) + df['Quarter'][i]
-
-
-    df['Year'] = df['Year'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m'))
-
-    year = df['Year']
-    const = df['Construction_costs']
-    material = df['Material_costs']
-    labour = df['Labour_costs']
-    house_price = df['House_price_index']
-    new_residential = df['New_residential_property']
-    existing_residential = df['Existing_residential_property']
-    mortgage = df['mortgage_rates']
+def EDA()
 
     st.markdown(f'<img src="https://www.barkowconsulting.com/img/barkow_consulting_logo.svg" alt="W3Schools.com">',unsafe_allow_html=True)
     #image = Image.open('imza_copy.png')
     #st.image(image, caption='Barkow Consulting GmbH')
-
-    
-
 
     st.header ("Missing values")
     st.markdown ("Finding 'missing values' is widely been in all pre-processing steps to identify null values in the data. With this, we get the number of missing records in each column")
@@ -78,12 +77,6 @@ def b():
     st.header("Describe")
     st.markdown ("The information gives a quick and simple description of the data. It can include Count, Mean, Standard Deviation, median, mode, minimum value, maximum value, range, standard deviation, etc. Statistics summary gives a high-level idea to identify whether the data has any outliers, data entry error, distribution of data such as the data is normally distributed or left/right skewed.")
     st.dataframe (df.describe(include="all"))
-
-    st.header("Data set")
-    st.dataframe(df)
-    st.markdown ("Source: https://www.destatis.de/EN/Themes/Economy/Prices/Construction-Prices-And-Real-Property-Prices/_node.html#267504")
-    st.markdown ("https://www.destatis.de/en/Themes/Economy/Prices/Construction-Prices-And-Real-Property-Prices/_node.html")
-    st.markdown ("https://www.bundesbank.de/dynamic/action/en/statistics/time-series-databases/time-series-databases/745582/745582?tsId=BBK01.SUD131&dateSelect=2023")
 
     st.header ("EDA Multivariate Analysis (Heatmap)")
     st.markdown ("As the name suggests, Multivariate analysis looks at more than two variables. Multivariate analysis is one of the most useful methods to determine relationships and analyze patterns for any dataset. A heat map is widely been used for Multivariate Analysis. Heat Map gives the correlation between the variables, whether it has a positive or negative correlation. In our example heat map shows the correlation between the variables.")
@@ -121,18 +114,11 @@ def b():
         st.markdown(f'<style>{file1.read()}</style>',unsafe_allow_html=True)
         st.text (file1.read())
 
-def c():
-    placeholder = st.empty()
-    placeholder.empty()
 
-
-
-
-
-options = st.sidebar.radio('Options', options = ['Introduction', 'B','C'])
+options = st.sidebar.radio('Options', options = ['Introduction', 'Dataset','EDA'])
 if options =='Introduction':
     introduction()
-elif options =='B':
-    b()
-elif options =='C':
-    c()
+elif options =='Dataset':
+    Dataset()
+elif options =='EDA':
+    EDA()
