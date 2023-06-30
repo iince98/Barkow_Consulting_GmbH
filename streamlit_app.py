54f308eca93abf17eeaa1f80e2eb98b20fabcb0b
I INCE <iince98@gmail.com>
2023-06-30 15:30:16 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,10 +100,6 @@ def EDA():
     fig_html1 = mpld3.fig_to_html(fig1)
     components.html(fig_html1, height=600)
 
- 
-    fig = px.line(df)
-    fig.show()
-
 
     fig, ax = plt.subplots(1,1)
     plt.plot(year,const,marker=".", markersize="5",color="r", label="construction")
